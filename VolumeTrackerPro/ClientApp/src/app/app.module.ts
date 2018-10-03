bb387550f3f1b5a94e782a5535dfbdbeca3cbad2
drowan9278 <drowan9278@students.mc3.edu>
2018-10-03 00:40:35 -0400

    Stable Changes, Currently Running on AWS

--- VolumeTrackerPro/ClientApp/src/app/app.module.ts
+++ VolumeTrackerPro/ClientApp/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
+import { CounterComponent } from './accountcreate/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
--- VolumeTrackerPro/Helpers/Class.cs
+++ VolumeTrackerPro/Helpers/Class.cs
@@ -12,19 +12,19 @@ namespace VolumeTrackerPro.Helpers
     {
         public static string GetRDSConnectionString()
         {
+            return null;
+            //var appConfig = ConfigurationManager.AppSettings;
 
-            var appConfig = IConfigurationManager.AppSettings;
+            //string dbname = appConfig["RDS_DB_NAME"];
 
-            string dbname = appConfig["RDS_DB_NAME"];
+            //if (string.IsNullOrEmpty(dbname)) return null;
 
-            if (string.IsNullOrEmpty(dbname)) return null;
+            //string username = appConfig["RDS_USERNAME"];
+            //string password = appConfig["RDS_PASSWORD"];
+            //string hostname = appConfig["RDS_HOSTNAME"];
+            //string port = appConfig["RDS_PORT"];
 
-            string username = appConfig["RDS_USERNAME"];
-            string password = appConfig["RDS_PASSWORD"];
-            string hostname = appConfig["RDS_HOSTNAME"];
-            string port = appConfig["RDS_PORT"];
-
-            return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
+            //return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
         }
 
     }
