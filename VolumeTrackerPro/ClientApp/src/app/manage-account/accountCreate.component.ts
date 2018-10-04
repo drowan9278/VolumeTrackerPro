d471f2554874e6472065157332447b65099e1182
drowan9278 <drowan9278@students.mc3.edu>
2018-10-03 23:46:56 -0400

    Added new manage account page

---
+++ VolumeTrackerPro/ClientApp/src/app/manage-account/accountCreate.component.ts
@@ -0,0 +1,13 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-accountCreate',
+  templateUrl: './accountCreate.component.html'
+})
+export class AccountCreateComponent {
+
+  constructor() {
+    
+  }
+}
